f67a80f3fd4f9f967f165d1ca3f226f7ac61f8fa
Warren Hansen <whansen1@mac.com>
2018-03-01 12:53:16 -0800

    initial commit

--- Channel Tests/MainViewController.swift
+++ Channel Tests/MainViewController.swift
@@ -5,21 +5,23 @@
 //  Created by Warren Hansen on 3/1/18.
 //  Copyright © 2018 Warren Hansen. All rights reserved.
 //
+//  [ ] Login firebase pods: firebase, realm
+//  [ ] request json
+//  [ ] parse json
+//  [ ] convert string to date
+//  [ ] save to realm as WeeklyStats (stringDate, date, profit, cumProfit, winPct, cost, ROI , annualRoi?, ticker, stars)
+//  [ ] market condition
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    
 
 }
 
